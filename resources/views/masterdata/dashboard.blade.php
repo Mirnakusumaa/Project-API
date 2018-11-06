b3832fed1129ed39db31c51f87b8d2f83093386e
mirnakusumaa <mirnakusumawati38@gmail.com>
2018-11-06 09:49:05 +0700

    add project

---
+++ resources/views/masterdata/dashboard.blade.php
@@ -0,0 +1,44 @@
+@extends('template.main')
+@section('content')
+<section id="slider" class="slider">
+            <div class="slider_overlay">
+                <div class="container">
+                    <div class="row">
+                        <div class="main_slider text-center">
+                            <div class="col-md-12">
+                                <div class="main_slider_content wow zoomIn" data-wow-duration="1s">
+                                    <img src="{{ asset('images/user.png') }}" alt="" />
+                                </div>
+                                <div class="col-md-offset-2">
+                                <div class="main_slider_content2 wow zoomIn col-md-12" data-wow-duration="1s">
+                                    <a href="pesan">
+                                        <img src="{{ asset('images/p.png') }}" alt="" />
+                                    </a>
+                                </div>
+                                    <div class="main_slider_content2 wow zoomIn col-md-12" data-wow-duration="1s">
+                                        <a href="confirm">
+                                            <img src="{{ asset('images/k.png') }}" alt="" />
+                                        </a>
+                                    </div>
+                                    <div class="main_slider_content2 wow zoomIn col-md-12" data-wow-duration="1s">
+                                        <a href="bayar">
+                                            <img src="{{ asset('images/b.png') }}" alt="" />
+                                        </a>
+                                    </div>
+                                    <div class="main_slider_content2 wow zoomIn col-md-12" data-wow-duration="1s">
+                                        <a href="status">
+                                            <img src="{{ asset('images/s.png') }}" alt="" />
+                                        </a>
+                                    </div> 
+                                        
+                                    </div>
+                                </div>
+
+                             
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+@endsection
